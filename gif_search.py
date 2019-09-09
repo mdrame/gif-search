c39a0d546c89c0b9b445c54ff061ee98472d6189
mohammed <mdrame113@gmail.com>
2019-09-09 13:55:01 -0400

    removed external html file by : mo

--- gif_search.py
+++ gif_search.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
-def index():
-    return render.template("site.html") # rander.tamplate is importing an external file.
+def func():
+    return '''
+    <!DOCTYPE html>
+<head>
+   <title>My title</title>
+   <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
+</head>
+<body style="width: 880px; margin: auto;">
+    <h1>Visible stuff goes here</h1>
+    <p>here's a paragraph, fwiw</p>
+    <p>And here's an image:</p>
+    <a href="https://www.flickr.com/photos/zokuga/14615349406/">
+        <img src="http://stash.compjour.org/assets/images/sunset.jpg" alt="it's a nice sunset">
+    </a>
+</body>
+    '''
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
