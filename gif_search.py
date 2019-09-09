f6458adc6a797eca7c164185386e516e270e9a32
mohammed <mdrame113@gmail.com>
2019-09-09 14:18:48 -0400

    render method not working

--- gif_search.py
+++ gif_search.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,21 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def func():
-    return '''
-    <!DOCTYPE html>
-<head>
-   <title>My title</title>
-   <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
-</head>
-<body style="width: 880px; margin: auto;">
-    <h1>Visible stuff goes here</h1>
-    <p>here's a paragraph, fwiw</p>
-    <p>And here's an image:</p>
-    <a href="https://www.flickr.com/photos/zokuga/14615349406/">
-        <img src="http://stash.compjour.org/assets/images/sunset.jpg" alt="it's a nice sunset">
-    </a>
-</body>
-    '''
+    return render_template('index.html') # render_template not working
 
 
 
