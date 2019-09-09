890970997c97fa187a037f8ad56bebfd17c32df1
mohammed <mdrame113@gmail.com>
2019-09-09 15:52:13 -0400

    added external css file by: mo

--- gif_search.py
+++ gif_search.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
 
+#todo: we nedda write a logic that will will check couple of charactor in the gif name and determine where they shoud be
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def func():
+
     return render_template('index.html') # render_template() added our external html file found in the template folder of the same folder
 
 
