ef3b8e0466d43d79c2915e704b8e1a896c213e22
mohammed <mdrame113@gmail.com>
2019-09-11 15:06:22 -0400

    testing basic flask

--- gif_search.py
+++ gif_search.py
@@ -1,15 +1,30 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
+import json
 
 #todo: we nedda write a logic that will will check couple of charactor in the gif name and determine where they shoud be
 
 app = Flask(__name__)
-
+gif_List = list()
 
 @app.route('/')
 def func():
 
-    return render_template('index.html') # render_template() added our external html file found in the template folder of the same folder
+
+
+    return render_template("index.html")
+     # TODO: Extract query term from url
+
+    # TODO: Make 'params' dict with query term and API key
+
+    # TODO: Make an API call to Tenor using the 'requests' library
+
+    # TODO: Get the first 10 results from the search results
+
+    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+
+
+ # render_template() added our external html file found in the template folder of the same folder
 
 
 
