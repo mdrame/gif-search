d4cff55a74934181bcebaeb6df0af23396491e30
mohammed <mdrame113@gmail.com>
2019-09-15 06:37:06 -0400

    API requesting working

--- gif_search.py
+++ gif_search.py
@@ -1,32 +1,38 @@
-from flask import Flask
-from random import choice
-from flask import render_template,request
+from flask import Flask, render_template,request
+import requests
+import json
 
-compliments = ['Dope', 'Cool','LIT 🔥', 'Awesome']
-#todo: we nedda write a logic that will will check couple of charactor in the gif name and determine where they shoud be
+
+
 
 app = Flask(__name__)
 gif_List = list()
 
 @app.route('/')
 def func():
-    compliment = choice(compliments)
-    return render_template('index.html', compliment=compliment)
-
-
 
      # TODO: Extract query term from url
+    user_Input = str(request.args.get('search'))
+
 
     # TODO: Make 'params' dict with query term and API key
+    params = {"url": 'https://api.tenor.com/v1/', "query": user_Input,"key": "SNXP25MEWK1E", "limit": 1}
+
+#testing API url
+# curl "https://api.tenor.com/v1/search?q=query&key=SNXP25MEWK1E&limit=10"
 
     # TODO: Make an API call to Tenor using the 'requests' library
-
+    api_requesting = requests.get(f"{params['url']}search?q={params['query']}&key={params['key']}&limit={params['limit']}")
+                                 #
+    api_results = api_requesting.json()
     # TODO: Get the first 10 results from the search results
+    gif = api_results["results"]
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
 
  # render_template() added our external html file found in the template folder of the same folder
+    return render_template('index.html',user_Input=user_Input, gif=gif)
 
 
 
